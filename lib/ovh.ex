f2aba4d6f048a6123dfd577d0ac16c0bd4a0c532
Jean Parpaillon <jean.parpaillon@free.fr>
2018-06-26 15:25:35 +0200

    Format code

--- lib/ovh.ex
+++ lib/ovh.ex
@@ -8,28 +8,33 @@ defmodule Ovh do
   @doc """
   Get a token
   """
-  @spec token() :: {:ok, Ovh.Token.t} | {:error, term}
+  @spec token() :: {:ok, Ovh.Token.t()} | {:error, term}
   def token() do
-    req_body = %{
-      "accessRules" => [ %{
-                           "method" => "GET",
-                           "path" => "/*"
-                         }
-                       ],
-      "redirection" => "http://api.ovh.com"
-    }
-    |> Poison.encode!()
-    req = {@url_auth,
-           [{'X-Ovh-Application', Application.get_env(:ovh, :app_key, "")}],
-           'application/json', req_body
-    }
+    req_body =
+      %{
+        "accessRules" => [
+          %{
+            "method" => "GET",
+            "path" => "/*"
+          }
+        ],
+        "redirection" => "http://api.ovh.com"
+      }
+      |> Poison.encode!()
+
+    req =
+      {@url_auth, [{'X-Ovh-Application', Application.get_env(:ovh, :app_key, "")}],
+       'application/json', req_body}
+
     case :httpc.request(:post, req, [], []) do
       {:ok, {{_, 200, _}, _, body}} ->
         body
         |> Poison.decode!()
         |> Ovh.Token.new()
+
       {:ok, {{_, code, err}, _, _}} ->
         {:error, {code, err}}
+
       {:error, err} ->
         {:error, err}
     end

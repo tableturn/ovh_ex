761745296f6ae4d7f8f7dc48e98e495bc2425f7b
Jean Parpaillon <jean.parpaillon@free.fr>
2018-09-11 15:36:10 +0200

    Upgrade version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ovh.MixProject do
   def project do
     [
       app: :ovh,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       aliases: [
